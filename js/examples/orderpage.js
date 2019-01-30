85719f1864b107b664092352483cc4ed8e8228ad
Ali Muhammad <alimsbaee@gmail.com>
2019-01-30 06:28:35 +0200

    updates

--- js/examples/orderpage.js
+++ js/examples/orderpage.js
@@ -7,11 +7,7 @@
     'use strict';
 
 
-
-    // basic
     $('.add-meal').on('click',function () {
-        
-        
         var modal = $(this).closest('.modal-block'),
             title = modal.find('.card-title').text(),
             size = modal.find('.meal-size').val(),
@@ -55,20 +51,61 @@
     });
 
     function newReceiptRow (qty,title,size,piecePrice) {
-        var record = $('<tr><td class="td-qty"><i class="fa fa-plus-circle qty-change-plus text-success"></i><span>'+qty+'</span><i class="fa fa-minus-circle qty-change-minus text-danger"></i></td> <td class="font-weight-bold td-title">'+title+'</td> <td class="td-size">'+size+'</td> <td class="td-price">'+qty*piecePrice+'</td> <td class="actions"> <a href="" class="delete-row text-danger"><i class="far fa-trash-alt"></i></a> </td> </tr>');
+        var record = $('<tr><td class="td-qty"><i class="fa fa-plus-circle qty-change-plus text-success"></i><span>'+qty+'</span><i class="fa fa-minus-circle qty-change-minus text-danger"></i></td> <td class="font-weight-bold td-title">'+title+'</td> <td class="td-size">'+size+'</td> <td class="td-price">'+qty*piecePrice+'</td> <td class="actions"> <a href="#reportOrder" class="ws-normal report-order"><i class="far fa-flag"></i></a><a href="#confirmDelete" class="ws-normal delete-row text-danger"><i class="far fa-trash-alt"></i></a> </td> </tr>');
         record.appendTo($('#receipt'));
     }
 
-    $('.customer-search-result').on('click',function () {
-        var name = $(this).find('p.title').text(),
-            phone = $(this).find('p.phone').text(),
-            address = $(this).find('p.address').text(),
-            customer = $('#customer-info');
 
-        customer.find('.name').text(name);
-        customer.find('.phone').text(phone);
-        customer.find('.address').text(address);
+
+
+    $(document).on('click','.delete-row',function () {
+        $(this).magnificPopup({
+            type: 'inline',
+            fixedContentPos: false,
+            fixedBgPos: true,
+            overflowY: 'auto',
+            closeBtnInside: true,
+            preloader: false,
+            midClick: true,
+            removalDelay: 300,
+            mainClass: 'my-mfp-zoom-in',
+            modal: true,
+        }).magnificPopup('open');
+
+        $(document).on('click', '.modal-confirm', function (e) {
+            e.preventDefault();
+            $.magnificPopup.instance.st.el.closest('tr').remove();
+            $.magnificPopup.close();
+        });
     });
+
+
+
+    $(document).on('click','.report-order',function () {
+        $(this).magnificPopup({
+            type: 'inline',
+            fixedContentPos: false,
+            fixedBgPos: true,
+            overflowY: 'auto',
+            closeBtnInside: true,
+            preloader: false,
+            midClick: true,
+            removalDelay: 300,
+            mainClass: 'my-mfp-zoom-in',
+            modal: true,
+        }).magnificPopup('open');
+
+        $(document).on('click', '.modal-confirm', function (e) {
+            e.preventDefault();
+            $.magnificPopup.instance.st.el.closest('tr').remove();
+            $.magnificPopup.close();
+        });
+    });
+
+
+
+
+
 
     $(document).on('click','.qty-change-plus',function () {
         var qty = parseInt($(this).siblings('span').text()),
@@ -82,7 +119,7 @@
         var qty = parseInt($(this).siblings('span').text()),
             tr = $(this).parents('tr'),
             piecePrice = parseInt(tr.find('.td-price').text())/qty;
-            if (qty !== 1) {
+        if (qty !== 1) {
             $(this).siblings('span').text(--qty);
             updatePrice(tr,piecePrice*qty);
         }
@@ -92,9 +129,101 @@
         target.find('.td-price').text(price);
     }
 
-    $(document).on('click','.delete-row',function () {
-        $(this).parents('tr').remove()
-    });
 
 
+
+
+
+
+
+
+
+
+
+
+
+    //Customer Search
+    var data = [{
+        id: 0,
+        text: '<p class="customer-name text-primary font-weight-bold m-0">Ali Muhammad</p>',
+        html: '<p class="customer-name text-primary h5 font-weight-bold m-0">Ali Muhammad</p><p class="customer-phone m-0"><i class="fas fa-phone-square mr-1"></i>01012537756</p><p class="customer-address m-0"><i class="fas fa-map-marker-alt mr-1"></i>Adab st Mansoura, Egypt</p>',
+        title: 'Ali Muhammad',
+        phone: '01012537756',
+        address: 'Adab st Mansoura, Egypt'
+    }, {
+        id: 1,
+        text: '<p class="customer-name text-primary font-weight-bold m-0">Ali Muhammad</p>',
+        html: '<p class="customer-name text-primary h5 font-weight-bold m-0">Ali Muhammad</p><p class="customer-phone m-0"><i class="fas fa-phone-square mr-1"></i>01012537756</p>',
+        title: 'Ali Muhammad',
+        phone: '01012537756',
+        children: [{
+            id: 2,
+            text: '<p class="customer-name text-primary font-weight-bold m-0">Ali Muhammad</p>',
+            html: '<p class="customer-address m-0 ml-3"><i class="fas fa-map-marker-alt mr-1"></i>Adab st Mansoura, Egypt</p>',
+            title: 'Ali Muhammad',
+            phone: '01012537756',
+            address: 'Adab st Mansoura, Egypt'
+        }, {
+            id: 3,
+            text: '<p class="customer-name text-primary font-weight-bold m-0">Ali Muhammad</p>',
+            html: '<p class="customer-address m-0 ml-3"><i class="fas fa-map-marker-alt mr-1"></i>Tahreer Square Cairo, Egypt</p>',
+            title: 'Ali Muhammad',
+            phone: '01012537756',
+            address: 'Tahreer Square Cairo, Egypt'
+        }]
+    }, {
+        id: 4,
+        text: '<p class="customer-name text-primary font-weight-bold m-0">Ali Muhammad</p>',
+        html: '<p class="customer-name text-primary h5 font-weight-bold m-0">Ali Muhammad</p><p class="customer-phone m-0"><i class="fas fa-phone-square mr-1"></i>01012537756</p><p class="customer-address m-0"><i class="fas fa-map-marker-alt mr-1"></i>Adab st Mansoura, Egypt</p>',
+        title: 'Ali Muhammad',
+        phone: '01012537756',
+        address: 'Adab st Mansoura, Egypt'
+    }, {
+        id: 5,
+        text: '<p class="customer-name text-primary font-weight-bold m-0">Ali Muhammad</p>',
+        html: '<p class="customer-name text-primary h5 font-weight-bold m-0">Ali Muhammad</p><p class="customer-phone m-0"><i class="fas fa-phone-square mr-1"></i>01012537756</p>',
+        title: 'Ali Muhammad',
+        phone: '01012537756',
+        children: [{
+            id: 6,
+            text: '<p class="customer-name text-primary font-weight-bold m-0">Ali Muhammad</p>',
+            html: '<p class="customer-address m-0 ml-3"><i class="fas fa-map-marker-alt mr-1"></i>Adab st Mansoura, Egypt</p>',
+            title: 'Ali Muhammad',
+            phone: '01012537756',
+            address: 'Adab st Mansoura, Egypt'
+        }, {
+            id: 7,
+            text: '<p class="customer-name text-primary font-weight-bold m-0">Ali Muhammad</p>',
+            html: '<p class="customer-address m-0 ml-3"><i class="fas fa-map-marker-alt mr-1"></i>Tahreer Square Cairo, Egypt</p>',
+            title: 'Ali Muhammad',
+            phone: '01012537756',
+            address: 'Tahreer Square Cairo, Egypt'
+        }]
+    }, {
+        id: 8,
+        text: '<p class="customer-name text-primary font-weight-bold m-0">Ali Muhammad</p>',
+        html: '<p class="customer-name text-primary h5 font-weight-bold m-0">Ali Muhammad</p><p class="customer-phone m-0"><i class="fas fa-phone-square mr-1"></i>01012537756</p><p class="customer-address m-0"><i class="fas fa-map-marker-alt mr-1"></i>Adab st Mansoura, Egypt</p>',
+        title: 'Ali Muhammad',
+        phone: '01012537756',
+        address: 'Adab st Mansoura, Egypt'
+    }];
+
+    $("#customerSearch").select2({
+        placeholder: "Select a customer",
+        allowClear: true,
+        data: data,
+        escapeMarkup: function(markup) {
+            return markup;
+        },
+        templateResult: function(data) {
+            return data.html;
+        },
+        templateSelection: function(data) {
+            return data.text;
+        },
+        theme: "bootstrap"
+    }).on('select2:select', function (e) {
+        $('#customerInfoPhone').text(e.params.data.phone);
+        $('#customerInfoAddress').text(e.params.data.address);
+    })
 }).apply(this, [jQuery]);
