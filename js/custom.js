a39d113dc20aba1bbeb85169829b6abe86f3ac30
Ali Muhammad <alimsbaee@gmail.com>
2019-02-06 05:44:25 +0200

    Discount

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,11 @@
 /* Add here all your JS customizations */
+(function ($) {
+    $('#ApplyDiscountCheckbox').on('change', function () {
+        var dropdown = $('#discountsDropdown');
+        if (this.checked) {
+            dropdown.fadeIn(100);
+        } else {
+            dropdown.fadeOut(100);
+        }
+    })
+}).apply(this, [jQuery]);
